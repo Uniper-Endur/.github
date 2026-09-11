da22302ecf41738b6ab3e8adcee66f7964f7f1d4
j38105-revu <144213926+j38105-revu@users.noreply.github.com>
2026-09-11 17:55:33 +0200

    Filter out '.github' repository from results

--- scripts/update-code-scanning-table.ps1
+++ scripts/update-code-scanning-table.ps1
@@ -34,7 +34,7 @@ if ($LASTEXITCODE -ne 0) {
     throw "Unable to retrieve repositories."
 }
 
-$repos = $reposJson | ConvertFrom-Json
+$repos = $reposJson | ConvertFrom-Json | Where-Object { $_.name -ne ".github" }
 
 Write-Host "Repositories found: $(@($repos).Count)"
 
